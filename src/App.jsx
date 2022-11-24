8121e383cafd1959e0bddb6412e932e0bc5265e7
siva <sivaifs4@gmail.com>
2022-11-24 15:53:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div >
+      Hello Chat app !!
+    </div>
+  );
+}
+
+export default App;
